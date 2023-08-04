c409d9ecc03f48f87b49db163e499be2f9663059
Маклашев Артем <maklashev_ak@outlook.com>
2023-08-04 18:47:19 +0400

    second commit HW2

--- src/main/java/view/menu/menuItems/MenuMethod.java
+++ src/main/java/view/menu/menuItems/MenuMethod.java
@@ -3,13 +3,13 @@ package view.menu.menuItems;
 import view.View;
 import view.menu.MenuItem;
 
-public abstract class MenuMethod implements MenuItem {
-    View view;
+// public abstract class MenuMethod implements MenuItem {
+//     View view;
 
-    public MenuMethod(View view) {
-        this.view = view;
-    }
-    public View getView() {
-        return this.view;
-    }
+//     public MenuMethod(View view) {
+//         this.view = view;
+//     }
+//     public View getView() {
+//         return this.view;
+//     }
 }
